602faf6603be60ce014063cf3ca3408522f634c4
ahbarnett <ahb@math.dartmouth.edu>
2019-08-29 12:43:36 -0400

    added interior case to GRF_conv 2D

--- 2D/GRF_conv.m
+++ 2D/GRF_conv.m
@@ -1,4 +1,4 @@
-function GRF_conv(tol,verb,curvemeth)
+function GRF_conv(tol,verb,curvemeth,interior)
 % QFS convergence test for GRF, 2D, one smooth curve. Compares to sing quad.
 %
 % GRF_conv(tol,verb) produces a plot in current figure.
@@ -16,14 +16,22 @@ if nargin<2, verb = 0; end
 o = []; o.factor='l';             % QFS opts
 o.verb = (verb>1);
 if nargin>=3, o.curvemeth=curvemeth; end
+if nargin<4, interior = false; end
 
 k = 0;                 % wavenumber (0 for now)
 a = .3; w = 5;         % smooth wobbly radial shape params
 
-interior = false;      % pull ext BVP data from Feb '19 early demo...
-% *** todo : interior case (choose pts)
-trg.x = 1.5-0.5i;      % far ext target point
-z0 = .2+0.7i; f0 = exp(1i*4.0);   % int src pt; BIE solved to emach @ N=480
+t0 = 1.042;            % bdry pt to base the rhs pt src (match z0=.2+.7i, Feb'19)
+% Here's Newton to solve for b.Z(t)=z0: t = t - (b.Z(t)-z0)./b.Zp(t), repeat!
+N0 = 250;              % desired N by which Nystrom (exp rate N) fully converged
+imt0 = -log(eps)/N0;              % imag dist of src so Nystrom emach at N=N0
+                                  % (or Kress SLP conv, at rate N/2, by N=2*N0)
+sgn = -1+2*interior;              % David convention (+1 if interior)
+b = wobblycurve(1,a,w,100);       % to access to b.Z
+z0 = b.Z(t0 - 1i*sgn*imt0);       % data src pt, given imag dist, sets conv rate
+if interior, trg.x = 0.3+0.2i;    % far int target point
+else trg.x = 1.5-0.5i; end        % far ext target point
+f0 = exp(1i*4.0);      % data strength to give data size O(1)
 if k==0                % Laplace
   fholom = @(z) f0./(z-z0);       % holomorphic ext soln, with no log nor const
   fpholom = @(z) -f0./(z-z0).^2;  % its complex deriv
@@ -31,11 +39,10 @@ if k==0                % Laplace
   fx = @(z) real(fpholom(z)); fy = @(z) -imag(fpholom(z)); % partials, NB sign!
   SLP = @LapSLP; DLP = @LapDLP;
 else    % ... Helm
-  SLP = @HelmSLP; DLP = @HelmDLP; % don't yet exist
+  SLP = @HelmSLP; DLP = @HelmDLP; % don't yet exist, *** need f too
 end
   
 srcker = SLP;                     % choose QFS src rep (*** fix when Helm)
-sgn = -1+2*interior;              % David convention (+1 if interior)
 
 Ns = 100:30:600;
 es = nan(1,numel(Ns)); cns=nan*Ns;                    % save errors, etc
@@ -51,7 +58,7 @@ for i=1:numel(Ns); N=Ns(i);       % ---------- N convergence
   ub = srcker(b,qs.s,co);          % eval the QFS rep (srces same locs)
   uerr = ub - f(b.x);              % compare to bdry potential
   es(1,i) = sqrt(sum(b.w .* abs(uerr).^2));           % QFS L_2 bdry err
-  ubk = SLP(b,b,sig) +  DLP(b,b,tau) + 0.5*tau;       % Kress sing quad PV + JR
+  ubk = SLP(b,b,sig) +  DLP(b,b,tau) - 0.5*sgn*tau;   % Kress sing quad PV + JR
   ukerr = ubk - f(b.x);
   es(2,i) = sqrt(sum(b.w .* abs(ukerr).^2));          % its L_2 bdry err
   es(3,i) = srcker(trg,qs.s,co) - f(trg.x);           % QFS far err
--- 2D/GRF_conv_multitol.m
+++ 2D/GRF_conv_multitol.m
@@ -1,12 +1,13 @@
 % driver for multiple GRF conv plots, 2D case
 % Barnett 8/15/19
 clear; verb = 1;
+interior = true;
 curvemeth = 'n';  % see GRF_conv.m
 tols = 10.^[-4:-2:-14];
 figure; set(gcf,'position',[100 100 1200 900]);
 for i=1:numel(tols)
   subplot(2,3,i);
-  GRF_conv(tols(i),verb,curvemeth);
+  GRF_conv(tols(i),verb,curvemeth,interior);
   drawnow
 end
 
