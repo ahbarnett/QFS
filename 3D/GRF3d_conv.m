68e6cc6ba727af3067eabd25e07ee59b4a72aa1a
ahbarnett <ahb@math.dartmouth.edu>
2019-09-08 23:16:06 -0400

    GRF3d_conv expts, surf GRF infty-norm not L2, readme todo

--- 3D/GRF3d_conv.m
+++ 3D/GRF3d_conv.m
@@ -10,8 +10,10 @@ function [Nvs es cns] = GRF3d_conv(shape,tol,verb,interior,o)
 %  interior - (boolean) whether test interior/exterior GRF
 %  o - opts struct, passed to qfs3d_create (see its opts)
 %
-% Example:
+% Examples:
 %  o.surfmeth='a'; o.param=1.0; o.factor='l'; GRF3d_conv(3,1e-9,2,false,o)
+%  o.surfmeth='d'; o.param=[1,0.12,1.5,0.27]; o.factor='b'; GRF3d_conv(4,1e-4,2,false,o)
+% o.surfmeth='d'; o.param=[1,0.1,1.5,0.2]; o.factor='b'; GRF3d_conv(0,1e-5,2,false,o)
 
 % Barnett 8/31/19, spheres 9/6/19
 if nargin<1, shape = 0; end
@@ -27,9 +29,10 @@ if ~isfield(o,'param'), o.param=[1,0.2,2.0,0.2]; end   % srcfac,ds,bfac,dc
 trg.x = [0.9;0.5;0.2];          % far int point (for tori)
 z0 = [1.8;-0.7;0.5]; f0=1.0;    % far ext pt
 
+qo = [];
 if shape==0, nam='plain torus';
   b = modulatedtorus(1.0,0.5);
-  Nvs = 20:5:35;
+  Nvs = 24:6:48;
 elseif shape==1, nam='cruller';
   b = modulatedtorus(1.0,cruller(0.5,0.1,5,3));
   Nvs = 40:5:60;
@@ -43,12 +46,13 @@ elseif shape==3, nam='sphere';
   Nvs = 16:8:48;
 elseif shape==4, nam='ellipsoid (aspect 2.5)';
   b = ellipsoid(.8,1.3,2);
+  %qo.minunodes = 20;   % ??
   trg.x = [0.3;-0.4;0.6];        % inside ellipsoid
-  Nvs = 30:5:50;
+  Nvs = 40:8:64;
 end
 if ~interior, f0 = 0.2; [z0 trg.x] = deal(trg.x,z0); end    % swap src<->trg
 
-if 0
+if 1         % edit : 0 if want just even m's excited, or 1 for all m's...
   f = @(x) f0./sqrt(sum((x-z0).^2,1));               % 1 pt src Laplace soln
   gradf = @(x) -f0*(x-z0)./sqrt(sum((x-z0).^2,1)).^3;
 else      % 2 equal pt sources, to kill the m=1 (in fact, all m odd) terms...
@@ -61,7 +65,8 @@ srcker = @Lap3dSLPmat;                     % choose QFS src rep
 sgn = sign_from_side(interior);
 es = nan(3,numel(Nvs)); cns=nan*Nvs;                  % save errors, etc
 for i=1:numel(Nvs); N=Nvs(i)*[2 1];       % --------- N convergence
-  b = setupsurfquad(b,N);                             % bdry nodes
+  b = setupsurfquad(b,N,qo);                          % bdry nodes
+  %b.w = b.w.*(1 + 1e-9*randn(size(b.w)));   % test perturbations!
   o.verb = 1;
   q = qfs3d_create(b,interior,{@Lap3dSLPmat,@Lap3dDLPmat},srcker,tol,o);
   qs = q{1}; qd = q{2};                               % get QFS for {S,D}LP
@@ -74,8 +79,11 @@ for i=1:numel(Nvs); N=Nvs(i)*[2 1];       % --------- N convergence
   cns(i) = sqrt(mean(abs(co).^2));                    % coeff rms
   ub = srcker(b,qs.s) * co;        % eval the QFS rep (srces same locs)
   uerr = ub - f(b.x)';             % compare to bdry potential
-  if verb>1, figure(3); clf; oo.nofig=1; showsurffunc(b,uerr,oo); title('GRF u err'); drawnow; end
-  es(1,i) = sqrt(sum(b.w .* sum(uerr.^2,1)));         % QFS L_2 bdry val err
+  if verb>1, figure(3); clf; oo.nofig=1; showsurffunc(b,uerr,oo); title('GRF u err');
+    figure(4); clf; oo.nofig=1; showsurffunc(qs.s,co,oo); title('QFS coeffs');
+    drawnow; end
+  %es(1,i) = sqrt(sum(b.w .* sum(uerr.^2)));           % QFS L_2 bdry val err
+  es(1,i) = max(abs(uerr));                    % QFS max bdry val err
   es(2,i) = srcker(trg,qs.s)*co - f(trg.x);           % QFS far val err
   es(3,i) = Lap3dSLPmat(trg,b)*sig + Lap3dDLPmat(trg,b)*tau - f(trg.x);  % native far val err
   fprintf('  GRF [%3d,%3d]: QFSsurf %.3g, QFSfar %.3g, nativefar %.3g, nrm:%.3g\n',N(1),N(2),es(1,i),es(2,i),es(3,i),cns(i))
--- README.md
+++ README.md
@@ -10,3 +10,12 @@
 For 2D, type `startup(2)`, or for 3D, `startup(3)`.
 Then try running some tests in the directory `2D` or `3D`.
 
+### To Do
+
+* variable-dist src choice 3D  
+* analyze ellipsoid focal ellipse singularities
+* try theta not z vertical param for sphere-like, diagnose sin(theta) m=1
+problems  
+* bigger GRF conv (int/ext) experiments
+* 3D singularity distance and convergence rate
+
